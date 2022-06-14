d556203c4e741b6d4cb5e7393e766d0b8673ddb3
areebhammad <areebhammad@gmail.com>
2022-06-14 13:46:05 +0530

    Testing

--- app.js
+++ app.js
@@ -9,11 +9,72 @@ const secText = section.querySelector('h1')
 
 //ScrollMagic 
 const controller = new ScrollMagic.Controller();
-const scene = new ScrollMagic.Scene({
-    duration: 9000,
+
+//Scene
+let scene = new ScrollMagic.Scene({
+    duration: 15000,
     triggerElement: intro,
     triggerHook: 0 
 })
 .addIndicators()
 .setPin(intro)
 .addTo(controller)
+
+//Text Animation
+
+const textAnime = TweenMax.fromTo(text, 3, { opacity: 1 }, { opacity: 0 });
+
+let scene2 = new ScrollMagic.Scene({
+    duration: 3000,
+    triggerElement: intro,
+    triggerHook: 0,
+})
+.setTween(textAnime)
+.addTo(controller)
+
+
+
+ 
+//Video Animation
+
+//accelAmount is something which we do for smoothing while scrolling through the video
+//if we scroll all of a sudden then the video will little bit more than where we stop to look smoothly
+// the lesser the amount the best smoothing we will get
+let accelAmount = 0.1;
+let scrollPosition = 0;
+let delay = 0;
+let Sc = ScrollMagic.scrollPos;
+
+scene.on("update",  (event) => {    //scrollPos is coming from ScrollMagic
+    scrollPosition = event.scrollPos / 1000;
+    //console.log(event);
+    
+    
+    setInterval(() => {
+        delay += (scrollPosition - delay) * accelAmount;
+        //console.log(scrollPosition, delay)
+        video.currentTime = delay;
+    },41.7)
+    
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//1000/24 = 41.666666667 fps
+//setInterval(() => {
+//    delay += (Sc - delay) * accelAmount;
+//   //console.log(scrollPosition, delay);
+////
+//    video.currentTime = delay;
+//},23.98);
